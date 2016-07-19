67c9c34712dd6c270290650b8868d3e26fff17c8
Sulaiman.Allen <xalerons@gmail.com>
2016-07-19 15:17:44 -0500

    Fixed spacing on behavior lexicon

--- lexicons/behavior_lexicon.py
+++ lexicons/behavior_lexicon.py
@@ -1,29 +1,48 @@
 behaviorDict = {
 
-  # Aggressive
-  ('military', 'extreme', 'confident', 'angry', 'evil', 'republican', 'cruel', 'cocky', 'strong', 'persistent', 'oppressive', 'wielded', 'conflict', 'cruelty', 'action', 'war', 'desire', 'oppression', 'impact', 'demand', 'raid', 'ruin', 'challenge', 'battle', 'argument'):'aggressive',
+    # Aggressive
+    ('military', 'extreme', 'confident', 'angry', 'evil', 'republican', 'cruel', 'cocky',
+        'strong', 'persistent', 'oppressive', 'wielded', 'conflict', 'cruelty', 'action',
+        'war', 'desire', 'oppression', 'impact', 'demand', 'raid', 'ruin', 'challenge',
+        'battle', 'argument'): 'aggressive',
 
-  # Passive
-  ('cautious', 'hopeful', 'prone', 'fearful', 'scared', 'fear', 'blessing'):'passive',
+    # Passive
+    ('cautious', 'hopeful', 'prone', 'fearful', 'scared', 'fear', 'blessing'): 'passive',
 
-  # Mentoring
-  ('inspiring', 'respect', 'share', 'development', 'ability', 'opinion', 'role', 'teaching'):'mentoring',
+    # Mentoring
+    ('inspiring', 'respect', 'share', 'development', 'ability', 'opinion', 'role',
+        'teaching'): 'mentoring',
 
-  # Inquisitive
-  ('interesting', 'psychological', 'sensible', 'truth', 'exploration', 'opinion', 'evidence', 'intelligence', 'wonder', 'fact', 'mind', 'conclusion', 'study', 'interest', 'mind', 'meaning', 'assumption', 'idea', 'experiment', 'debate', 'thought'):'inquisitive',
+    # Inquisitive
+    ('interesting', 'psychological', 'sensible', 'truth', 'exploration', 'opinion',
+        'evidence', 'intelligence', 'wonder', 'fact', 'mind', 'conclusion', 'study',
+        'interest', 'mind', 'meaning', 'assumption', 'idea', 'experiment', 'debate',
+        'thought'): 'inquisitive',
 
-  # Transaction
-  ('generous', 'lucky', 'worth', 'honored', 'filled', 'rewarded', 'satisfied', 'attainment', 'promise', 'opportunity', 'goal', 'offer', 'credit', 'money', 'gain', 'proceeding', 'share', 'job', 'service', 'experiment', 'task', 'relation', 'retrieve', 'validate', 'livelihood'):'transaction',
+    # Transaction
+    ('generous', 'lucky', 'worth', 'honored', 'filled', 'rewarded', 'satisfied',
+        'attainment', 'promise', 'opportunity', 'goal', 'offer', 'credit', 'money',
+        'gain', 'proceeding', 'share', 'job', 'service', 'experiment', 'task',
+        'relation', 'retrieve', 'validate', 'livelihood'): 'transaction',
 
-  # Encouragement
-  ('funny', 'sweet', 'good', 'positive', 'great', 'happy', 'eager', 'greatest', 'satisfied', 'respect', 'love', 'enthusiasm', 'positivity', 'confidence', 'felicity', 'validate'):'encouragement',
+    # Encouragement
+    ('funny', 'sweet', 'good', 'positive', 'great', 'happy', 'eager', 'greatest',
+        'satisfied', 'respect', 'love', 'enthusiasm', 'positivity', 'confidence',
+        'felicity', 'validate'): 'encouragement',
 
-  # Comparison
-  ('similar', 'possible', 'many', 'new', 'little', 'widespread', 'last', 'much', 'different', 'able', 'many', 'absurd', 'richest', 'best', 'least', 'greatest', 'increase', 'relationship'):'comparison',
+    # Comparison
+    ('similar', 'possible', 'many', 'new', 'little', 'widespread', 'last', 'much',
+        'different', 'able', 'many', 'absurd', 'richest', 'best', 'least', 'greatest',
+        'increase', 'relationship'): 'comparison',
 
-  # Spiritual
-  ('spiritual', 'emotional', 'self-knowledge', 'personal', 'love', 'belief', 'soul', 'journey', 'creation', 'freedom', 'emotion', 'dream', 'miracle', 'blessing', 'passion'):'spiritual',
+    # Spiritual
+    ('spiritual', 'emotional', 'self-knowledge', 'personal', 'love', 'belief', 'soul',
+        'journey', 'creation', 'freedom', 'emotion', 'dream', 'miracle', 'blessing',
+        'passion'): 'spiritual',
 
-  # Frustration
-  ('negative', 'overwhelming', 'hard', 'depressed', 'doubtful', 'terrible', 'sorry', 'judgmental', 'morbid', 'foolish', 'tortured', 'sickened', 'vexation', 'disappointment', 'trouble', 'resistance', 'ruin', 'job', 'challenge', 'negativity', 'misdeed'):'frustration'
+    # Frustration
+    ('negative', 'overwhelming', 'hard', 'depressed', 'doubtful', 'terrible', 'sorry',
+        'judgmental', 'morbid', 'foolish', 'tortured', 'sickened', 'vexation', 'misdeed',
+        'disappointment', 'trouble', 'resistance', 'ruin', 'job', 'challenge',
+        'negativity'): 'frustration'
 }
