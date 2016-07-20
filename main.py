5c083b478b0f81d8ef37cd74b5f283b59e9402a8
Sulaiman.Allen <xalerons@gmail.com>
2016-07-20 14:51:58 -0500

    Switched sets to tuple

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ class Analyze:
                         # print(resultDict[behaviorDict[key]])
 
         for key in resultDict:
-            resultList.append({key, round(resultDict[key] / denominator, 4)})
+            resultList.append((key, round(resultDict[key] / denominator, 4)))
 
         return resultList
 
@@ -133,7 +133,7 @@ class Analyze:
                         # print(resultDict[behaviorDict[key]])
 
         for key in resultDict:
-            resultList.append({key, round(resultDict[key] / denominator, 4)})
+            resultList.append((key, round(resultDict[key] / denominator, 4)))
 
         # for word in word_list:
         #     for key in domainDict:
