78a18507f42814425227656fd8562ec42a9fa41e
Russell Ries <riesru@gmail.com>
2016-07-21 10:41:06 -0500

    add Easter Eggs to behavior lexicon

--- lexicons/behavior_lexicon.py
+++ lexicons/behavior_lexicon.py
@@ -4,10 +4,10 @@ behaviorDict = {
     ('military', 'extreme', 'confident', 'angry', 'evil', 'republican', 'cruel', 'cocky',
         'strong', 'persistent', 'oppressive', 'wielded', 'conflict', 'cruelty', 'action',
         'war', 'desire', 'oppression', 'impact', 'demand', 'raid', 'ruin', 'challenge',
-        'battle', 'argument', '!', 'hate', 'fuck'): 'aggressive',
+        'battle', 'argument', 'Patrick', '!', 'hate', 'fuck'): 'aggressive',
 
     # Passive
-    ('cautious', 'hopeful', 'prone', 'fearful', 'scared', 'fear', 'blessing', 'quiet', 'static', 'uninvolved', 'flat', 'idle', 'patient', 'compliant', 'docile', 'inert', 'latent', 'receptive', 'resigned', 'stolid', 'submissive', 'tractable', 'unassertive', 'indifferent', 'languid', 'stoic', 'apathetic'): 'passive',
+    ('cautious', 'hopeful', 'prone', 'fearful', 'scared', 'fear', 'blessing', 'quiet', 'static', 'uninvolved', 'flat', 'idle', 'patient', 'compliant', 'docile', 'inert', 'latent', 'receptive', 'resigned', 'stolid', 'Hannah', 'submissive', 'tractable', 'unassertive', 'indifferent', 'languid', 'stoic', 'apathetic'): 'passive',
 
     # Mentoring
     ('inspiring', 'respect', 'share', 'development', 'ability', 'opinion', 'role',
@@ -38,7 +38,7 @@ behaviorDict = {
     # Spiritual
     ('spiritual', 'emotional', 'self-knowledge', 'personal', 'love', 'belief', 'soul',
         'journey', 'creation', 'freedom', 'emotion', 'dream', 'miracle', 'blessing',
-        'passion', 'divine', 'holy', 'sacred', 'intangible', 'ethereal', 'ghost', 'pure', 'rarefied', 'faith', 'praise', 'pray', 'god', 'angel', 'preacher', 'rabbi', 'cleric', 'priest', 'lama', 'friar', 'pontiff'): 'spiritual',
+        'passion', 'divine', 'holy', 'sacred', 'intangible', 'ethereal', 'ghost', 'pure', 'rarefied', 'faith', 'praise', 'pray', 'Russell', 'god', 'angel', 'preacher', 'rabbi', 'cleric', 'priest', 'lama', 'friar', 'pontiff'): 'spiritual',
 
     # Frustration
     ('negative', 'overwhelming', 'hard', 'depressed', 'doubtful', 'terrible', 'sorry',
