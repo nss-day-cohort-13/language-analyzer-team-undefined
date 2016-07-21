e29ba496f447ca072d65073710a5ff88cd46fd5e
Russell Ries <riesru@gmail.com>
2016-07-21 10:38:05 -0500

    add more words to behavior lexicon

--- lexicons/behavior_lexicon.py
+++ lexicons/behavior_lexicon.py
@@ -4,45 +4,45 @@ behaviorDict = {
     ('military', 'extreme', 'confident', 'angry', 'evil', 'republican', 'cruel', 'cocky',
         'strong', 'persistent', 'oppressive', 'wielded', 'conflict', 'cruelty', 'action',
         'war', 'desire', 'oppression', 'impact', 'demand', 'raid', 'ruin', 'challenge',
-        'battle', 'argument', '!'): 'aggressive',
+        'battle', 'argument', '!', 'hate', 'fuck'): 'aggressive',
 
     # Passive
-    ('cautious', 'hopeful', 'prone', 'fearful', 'scared', 'fear', 'blessing'): 'passive',
+    ('cautious', 'hopeful', 'prone', 'fearful', 'scared', 'fear', 'blessing', 'quiet', 'static', 'uninvolved', 'flat', 'idle', 'patient', 'compliant', 'docile', 'inert', 'latent', 'receptive', 'resigned', 'stolid', 'submissive', 'tractable', 'unassertive', 'indifferent', 'languid', 'stoic', 'apathetic'): 'passive',
 
     # Mentoring
     ('inspiring', 'respect', 'share', 'development', 'ability', 'opinion', 'role',
-        'teaching'): 'mentoring',
+        'teaching', 'coach', 'counsel', 'guide', 'instruct', 'edify', 'educate', 'explain', 'teach', 'tutor', 'aid', 'champion', 'help', 'sponsor'): 'mentoring',
 
     # Inquisitive
-    ('interesting', 'psychological', 'sensible', 'truth', 'exploration', 'opinion',
+    ('interest', 'psychological', 'sensible', 'truth', 'exploration', 'opinion',
         'evidence', 'intelligence', 'wonder', 'fact', 'mind', 'conclusion', 'study',
         'interest', 'mind', 'meaning', 'assumption', 'idea', 'experiment', 'debate',
-        'thought', 'who', 'what', 'when', 'where', 'why', 'how', '?'): 'inquisitive',
+        'thought', 'who', 'what', 'when', 'where', 'why', 'how', '?', 'analytical', 'nosy', 'investigate', 'scrutinize', 'pry', 'learn'): 'inquisitive',
 
     # Transaction
     ('generous', 'lucky', 'worth', 'honored', 'filled', 'rewarded', 'satisfied',
         'attainment', 'promise', 'opportunity', 'goal', 'offer', 'credit', 'money',
         'gain', 'proceeding', 'share', 'job', 'service', 'experiment', 'task',
-        'relation', 'retrieve', 'validate', 'livelihood'): 'transaction',
+        'relation', 'retrieve', 'validate', 'livelihood', 'action', 'bond', 'business', 'buying','contract', 'deal', 'enterprise', 'negotiation', 'purchase', 'sale', 'bargain', 'compact', 'convention', 'deed'): 'transaction',
 
     # Encouragement
     ('funny', 'sweet', 'good', 'positive', 'great', 'happy', 'eager', 'greatest',
         'satisfied', 'respect', 'love', 'enthusiasm', 'positivity', 'confidence',
-        'felicity', 'validate'): 'encouragement',
+        'felicity', 'validate', 'cheer', 'backing', 'comfort', 'faith', 'inspiration', 'advocacy', 'aid', 'trust', 'hope', 'reassure', 'praise'): 'encouragement',
 
     # Comparison
     ('similar', 'possible', 'many', 'new', 'little', 'widespread', 'last', 'much',
         'different', 'able', 'many', 'absurd', 'richest', 'best', 'least', 'greatest',
-        'increase', 'relationship'): 'comparison',
+        'increase', 'relationship', 'resemblance', 'semblance', 'likeness', 'relation', 'like', 'near', 'far', 'above', 'below', 'under', 'top', 'bottom', 'close'): 'comparison',
 
     # Spiritual
     ('spiritual', 'emotional', 'self-knowledge', 'personal', 'love', 'belief', 'soul',
         'journey', 'creation', 'freedom', 'emotion', 'dream', 'miracle', 'blessing',
-        'passion'): 'spiritual',
+        'passion', 'divine', 'holy', 'sacred', 'intangible', 'ethereal', 'ghost', 'pure', 'rarefied', 'faith', 'praise', 'pray', 'god', 'angel', 'preacher', 'rabbi', 'cleric', 'priest', 'lama', 'friar', 'pontiff'): 'spiritual',
 
     # Frustration
     ('negative', 'overwhelming', 'hard', 'depressed', 'doubtful', 'terrible', 'sorry',
         'judgmental', 'morbid', 'foolish', 'tortured', 'sickened', 'vexation', 'misdeed',
         'disappointment', 'trouble', 'resistance', 'ruin', 'job', 'challenge',
-        'negativity'): 'frustration'
+        'negativity', 'failure', 'dissatisfaction', 'irritation', 'resentment', 'setback', 'hindrance', 'Sule', 'impediment', 'defeat', 'obstruction', 'downer', 'disaster', 'dismay', 'upset', 'humiliation', 'vexation'): 'frustration'
 }
